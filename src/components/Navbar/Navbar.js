45859903093e1f9c9496bc748153e0bf6cfb5cc6
davinc131 <davinc131@hotmail.com>
2022-04-17 12:19:09 -0300

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import "./navbar.css";
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <span>React Kanban</span>
+    </nav>
+  );
+}
---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TaskItem from "../TaskItem/TaskItem";
+import PlusIcon from "../../img/plus-svgrepo-com.svg";
+import "./tasklist.css";
+
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Nova tarefa", taskState);
+  };
+
+  return (
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks?.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+
+        {tasks.length === 0 && <div className="empty-list">Lista vazia</div>}
+
+        <button onClick={addTask} className="btn">
+          <img src={PlusIcon} alt="plus" />
+          Adicionar Tarefa
+        </button>
+      </div>
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
+};
